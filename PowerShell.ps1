ef8808fa61f4f2018e1774e24369f97537e08e73
JiaBaili05 <jiabaili05@outlook.com>
2025-03-09 12:39:07 +0800

    Add files via upload

---
+++ PowerShell.ps1
@@ -0,0 +1,1 @@
+Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `"irm https://massgrave.dev/get | iex`"" -Verb RunAs
---
+++ Windows或者Office激活.bat
@@ -0,0 +1,2 @@
+@echo off
+PowerShell -NoProfile -ExecutionPolicy Bypass -File %~dp0PowerShell.ps1
